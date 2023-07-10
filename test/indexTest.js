b635add14627b99668248e75485c6260ba2c86b7
robertnjunge <mubearobert@gmail.com>
2023-07-10 22:14:53 +0300

    completed assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,8 @@
 require ( './root.js' );
 
+const { expect } = require('chai');
 const { name, height, message } = require("../index.js");
+const { describe } = require('mocha');
 
 /*
 describe('what-is-a-test', () => {
@@ -29,10 +31,21 @@ describe('what-is-a-test', () => {
 // The code below ensures that students who are using CodeGrade will get credit 
 // for the code-along in Canvas; you can disregard it.
 
-describe('', () => {
-  describe('', () => {
-    it('', () => {
+describe("Name", () => {
+  it('returns"Susan"',() =>{
+    expect(name).toEqual("Susan");
+      });
+    });
+describe("Height", () => {
+  it("is less than 40", () =>{
+    expect(height).tobelessthan(40);
+  });
+
+  describe('Message', () => {
+    it("gives the name and height", () => {
       return true;
+       expect(message).toInclude(name);
+       expect(message).toInclude(height);
     })
   })
 })
